e87ef095555a276ac0a0faecdca2f548d049d9f1
Matthew Miner <misternuks@gmail.com>
2023-05-09 15:50:30 +0900

    complete routes and controller, and build views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # CREATE
+  # Display the form:
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  # Handle the POST request:
+  post '/tasks', to: 'tasks#create', as: :tasks
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # READ
+  # List all tasks
+  get '/tasks', to: 'tasks#index'
+
+  # list one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # UPDATE
+  # List the task to edit
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # Update it
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # DESTROY
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
